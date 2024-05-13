c9078227d185bedb1dea1e66e64448a8214eae36
suntaeyoung <sty8706@naver.com>
2024-05-13 09:59:11 +0900

    프로젝트 demo 주소들 변경

--- src/pages/projectData/projectData.js
+++ src/pages/projectData/projectData.js
@@ -4,28 +4,28 @@ const projectData = [
     image: "/images/projectScreen/googleform.png",
     description: "Vanilla JS로 구현한 GoogleForm 웹앱",
     githubLink: "https://github.com/suntaeyoung/vanilla-googleform-spa",
-    demoLink: "https://vanilla-googleform-spa.vercel.app/"
+    demoLink: "https://vanilla-googleform-spa.netlify.app/"
   },
   {
     title: "TODO List - JS버전",
     image: "/images/projectScreen/todo-JS.png",
     description: "Vanilla JS로 구현한 TODO List 웹앱",
     githubLink: "https://github.com/suntaeyoung/vanilla-todolist",
-    demoLink: "https://vanilla-todolist-sable.vercel.app/"
+    demoLink: "https://ty-vanilla-todolist.netlify.app/"
   },
   {
     title: "TODO List - React버전",
     image: "/images/projectScreen/todo-React.png",
     description: "React를 활용한 TODO List 웹앱",
     githubLink: "https://github.com/suntaeyoung/react-todo",
-    demoLink: "https://react-todo-theta-sand.vercel.app/"
+    demoLink: "https://ty-react-todolist.netlify.app/"
   },
   {
     title: "First Portfolio Website",
     image: "/images/projectScreen/self-introduction.png",
     description: "첫번째 자기소개서 포트폴리오",
     githubLink: "https://github.com/suntaeyoung/suntaeyoung_self-introduction",
-    demoLink: "https://suntaeyoung-self-introduction.vercel.app/"
+    demoLink: "https://suntaeyoung-self-introduction.netlify.app/"
   },
 ];
 
